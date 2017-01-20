54d49af144f1aaeddc3d0bd254d1f8c9d27b7d2c
Mace <mason@gls.co.za>
2017-01-20 14:24:28 +0200

    Initial Planet Spawner Commit

---
+++ Assets/Scripts/PlanetSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetSpawner : MonoBehaviour 
+{
+
+	public GameObject[] Planets;
+	public PlanetPosition[] Positions;
+
+	// Use this for initialization
+	void Start () 
+	{
+		int randPosition = Random.Range(0, 4);
+		int randPlanet = Random.Range(0, 4);
+
+		Vector3 spawnPosition = Positions[randPosition].transform.position;
+		Instantiate(Planets[randPlanet], spawnPosition, Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
