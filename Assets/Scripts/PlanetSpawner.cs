b5cba363f64ec4f06c7f7e24da7ddff976cf3082
Mace <mason@gls.co.za>
2017-01-20 15:58:37 +0200

    Added a planet shredder

--- Assets/Scripts/PlanetSpawner.cs
+++ Assets/Scripts/PlanetSpawner.cs
@@ -8,20 +8,35 @@ public class PlanetSpawner : MonoBehaviour
 	public GameObject[] Planets;
 	public PlanetPosition[] Positions;
 
+	private GameObject SpawnedPlanet;
+	private Transform PositionTransform;
+
 	// Use this for initialization
 	void Start () 
 	{
-		int randPosition = Random.Range(0, 4);
-		int randPlanet = Random.Range(0, 4);
-
-		Transform positionTransform = Positions[randPosition].transform;
-		GameObject spawnedPlanet = (GameObject)Instantiate(Planets[randPlanet], positionTransform.position, Quaternion.identity);
-		spawnedPlanet.transform.SetParent(positionTransform);
+		SpawnPlanet();
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		
+		if(!PositionTransform && !SpawnedPlanet)
+		{
+			SpawnPlanet();
+		}
 	}
+
+	void SpawnPlanet()
+	{
+		int randPosition = Random.Range(0, 4);
+		int randPlanet = Random.Range(0, 4);
+
+		if(!Positions[randPosition])
+			SpawnPlanet();
+		
+		PositionTransform = Positions[randPosition].transform;
+		SpawnedPlanet = (GameObject)Instantiate(Planets[randPlanet], PositionTransform.position, Quaternion.identity);
+		SpawnedPlanet.transform.SetParent(PositionTransform);
+	}
+
 }
