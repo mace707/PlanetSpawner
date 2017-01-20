a70d22f7c705f5ba182a797cb78b9bf41a44138e
Mace <mason@gls.co.za>
2017-01-20 14:30:34 +0200

    Planet is a child of Position.

--- Assets/Scripts/PlanetSpawner.cs
+++ Assets/Scripts/PlanetSpawner.cs
@@ -14,8 +14,9 @@ public class PlanetSpawner : MonoBehaviour
 		int randPosition = Random.Range(0, 4);
 		int randPlanet = Random.Range(0, 4);
 
-		Vector3 spawnPosition = Positions[randPosition].transform.position;
-		Instantiate(Planets[randPlanet], spawnPosition, Quaternion.identity);
+		Transform positionTransform = Positions[randPosition].transform;
+		GameObject spawnedPlanet = (GameObject)Instantiate(Planets[randPlanet], positionTransform.position, Quaternion.identity);
+		spawnedPlanet.transform.SetParent(positionTransform);
 	}
 	
 	// Update is called once per frame
